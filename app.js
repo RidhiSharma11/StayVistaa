16de4ebf4bd4edd49394f4c6940b4272324fb91f
Ridhi Sharma <theridhiisharmaa@gmail.com>
2025-10-18 18:07:24 +0530

    Index, Read Route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
+const path=require("path");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/StayVistaa"
 
@@ -17,23 +18,39 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
     res.send("Hey! I am the root");
 })
 
-app.get("/testlisting",async(req,res)=>{
-     let sampletesting=new Listing({
-      title:"My new Villa",
-      description:"By the Beach",
-      price:1200,
-      location:"Calangute,Goa",
-      country:"India",
-  });
-  await sampletesting.save();
-  console.log("Database was saved");
-  res.send("Successful Testing");
-});
+//Index Route
+app.get("/listings",async(req,res)=>{
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs",{allListings});
+})
+
+//Show Route
+app.get("/listings/:id",async(req,res)=>{
+  let {id}=req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs",{listing});
+})
+
+// app.get("/testlisting",async(req,res)=>{
+//      let sampletesting=new Listing({
+//       title:"My new Villa",
+//       description:"By the Beach",
+//       price:1200,
+//       location:"Calangute,Goa",
+//       country:"India",
+//   });
+//   await sampletesting.save();
+//   console.log("Database was saved");
+//   res.send("Successful Testing");
+// });
 
 app.listen(6060,()=>{
     console.log("Port is working succesfully");
--- models/listing.js
+++ models/listing.js
@@ -7,11 +7,17 @@ const listSchema=new Schema({
         default:true,
     },
     description:String,
-    image:{
-        type:String,
-        default:"https://images.unsplash.com/photo-1720884413532-59289875c3e1?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735",
-        set:(v)=>v===""?"https://images.unsplash.com/photo-1720884413532-59289875c3e1?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735":v,
-    },
+image: {
+  filename: {
+    type: String,
+    default: "listingimage"
+  },
+  url: {
+    type: String,
+    default: "https://images.unsplash.com/photo-1720884413532-59289875c3e1?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735"
+  }
+},
+
     price:Number,
     location:String,
     country:String,
