e80e628e8d040feab57a9830b4c1655aec1e7d52
Ridhi Sharma <theridhiisharmaa@gmail.com>
2026-04-21 16:39:56 +0530

    integrated reviews

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const expressError = require("./utils/expressError.js");
-const {listingSchema} = require("./schema.js");
+const {listingSchema,reviewSchema} = require("./schema.js");
+const Review = require("./models/review.js");
+const review = require("./models/review.js");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/StayVistaa"
 
@@ -38,6 +40,17 @@ app.get("/", wrapAsync(async (req, res) => {
 
 const validateListing = (req,res,next) =>{
   let {error} = listingSchema.validate(req.body);
+  if(error){
+    let errMsg = error.details.map((el)=>el.message).join(",");
+    throw new expressError(400, errMsg);
+  }
+  else{
+    next();
+  }
+};
+
+const validateReview = (req,res,next) =>{
+  let {error} = reviewSchema.validate(req.body);
   if(error){
     let errMsg = error.details.map((el)=>el.message).join(",");
     throw new expressError(400, errMsg);
@@ -78,8 +91,9 @@ app.get("/listings/new",wrapAsync(async(req,res)=>{
 //Show Route
 app.get("/listings/:id",wrapAsync(async(req,res)=>{
   let {id}=req.params;
-  const listing = await Listing.findById(id);
+  const listing = await Listing.findById(id).populate("reviews");
   res.render("listings/show.ejs",{listing});
+  console.log(listing.reviews);
 }));
 
 //Create Route
@@ -113,6 +127,29 @@ app.delete("/listings/:id",wrapAsync(async(req,res)=>{
   res.redirect("/listings");
 }));
 
+//Reviews
+//Post Routes
+app.post("/listings/:id/reviews", validateReview,
+  wrapAsync(async(req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+    await newReview.save();          
+    listing.reviews.push(newReview._id);
+    await listing.save();
+    res.redirect(`/listings/${listing._id}`);
+    console.log("BODY:", req.body);
+}));
+
+//Review Delete Post
+app.delete(
+  "/listings/:id/reviews/:reviewId",
+  wrapAsync(async(req,res)=>{
+    let{id,reviewId}=req.params;
+    await Listing.findByIdAndUpdate(id,{$pull:{reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+  })
+  );
 
 app.use((req,res,next)=>{
   next(new expressError(404,"Page not found"));
--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,7 @@
 const mongoose=require("mongoose");
+const { listingSchema } = require("../schema");
 const Schema=mongoose.Schema;
+const Review = require("./review.js");
 
 const listSchema=new Schema({
     title:{
@@ -21,7 +23,19 @@ image: {
     price:Number,
     location:String,
     country:String,
+    reviews: [
+      {
+        type:Schema.Types.ObjectId,
+        ref:"Review"
+      },
+    ],
 });
+
+// listingSchema.post("findOneAndDelete",async(listing)=>{
+//   if(listing){
+//     await Review.deleteMany({_id: { $in: listing.reviews}});
+//   }
+// });
 
 const Listing=mongoose.model("Listing",listSchema);
 module.exports=Listing;
